6e4d7d75cd62a90703075e736eea0ace8e01cacc
Bryce Vandegrift <brycevandegrift@airmail.cc>
2021-03-01 10:29:28 -0500

    Made icon changes

--- bsd.c
+++ bsd.c
@@ -33,20 +33,20 @@ int main()
 
 	if (strcmp(os, "FreeBSD\n") == 0)
 	{
-		printf("/\\,-'''''-,/\\		User: %s", user);
-		printf("\\_)       (_/		\n");
+		printf("/\\\\,-'''''-,/\\\\		User: %s", user);
+		printf("\\\\_)       (_/		\n");
 		printf("|           |		Kernel: %s", kernel);
 		printf("|           |		\n");
 		printf(" ;         ;		\n");
 		printf("  '-_____-'		\n");
 	} else if(strcmp(os, "OpenBSD\n") == 0) { 
 		printf("       _____      	User: %s", user);
-		printf("     \-     -/    	\n");
-		printf("  \_/         \   	Kernel: %s", kernel);
+		printf("     \\-     -/    	\n");
+		printf("  \\_/         \\   	Kernel: %s", kernel);
 		printf("  |        O O |  	\n");
 		printf("  |_  <   )  3 )  	\n");
-		printf("  / \         /   	\n");
-		printf("     /-_____-\    	\n");
+		printf("  / \\         /   	\n");
+		printf("     /-_____-\\    	\n");
 	} else
 	{
 		printf("Sorry your OS is not supported. Please open a issue at https://github.com/ncvfg234560/fetchr. :(\n");
