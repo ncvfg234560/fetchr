acc7acdaf4e24a3312eb6158684ec952453611fe
Bryce Vandegrift <brycevandegrift@airmail.cc>
2021-03-03 13:32:54 -0500

    Added Debian Support

---
+++ debian.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+char user[20];
+char host[20];
+char kernel[20];
+char os[10];
+FILE *temp;
+
+int main()
+{
+	system("whoami >> tmp");
+	system("uname -s >> tmp");
+	system("uname -r >> tmp");
+	temp = fopen("tmp", "r");
+	fgets(user, 20, temp);
+	fgets(os, 10, temp);
+	fgets(kernel, 20, temp);
+	fclose(temp);
+	system("rm tmp");
+
+	printf("    _____     User: %s", user);
+	printf("   /  __ \\   \n");
+	printf("  |  /    |   Kernel: %s", kernel);
+	printf("  |  \\___-   \n");
+	printf("  -_          \n");
+	printf("    --_       \n");
+	printf("              \n");
+
+	return 0;
+}
